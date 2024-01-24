fbc89e8f66f763641bd2de908c3905397e5b240d
Emily Ochoa <eochoa156@gmail.com>
2024-01-23 17:39:41 -0800

    added sword

--- index.js
+++ index.js
@@ -51,5 +51,19 @@ newImage('assets/pine-tree.png', '450px', '200px');
 newImage('assets/tree.png', '200px', '300px');
 newImage('assets/pillar.png', '350px', '100px')
 newImage('assets/crate.png', '150px', '200px');
-newImage('assets/well.png', '500px', '425px')
+newImage('assets/well.png', '500px', '425px');
+
+function newItem(swordSrc, swordLeft, swordBottom, ) {
+    let sword = document.createElement('img');
+    sword.src = swordSrc;
+    sword.style.position = 'fixed';
+    sword.style.left = swordLeft;
+    sword.style.bottom = swordBottom;
+    document.body.append(sword);
+
+    sword.addEventListener('click', function() {
+        sword.remove()
+    })
+}
+newItem('assets/sword.png', '500px', '405px', '200px')
 
